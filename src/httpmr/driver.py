a62bf3b6e07d94a095de005be1159088889d8e92
peterjdolan <peterjdolan@c6a7e3dd-4d4d-0410-b626-13292664cdac>
2008-07-14 00:15:10 +0000

    Setting up the Driver to collect statistics from the Map and Reduce operation pages.

--- src/httpmr/driver.py
+++ src/httpmr/driver.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
 
+"""Simple multithreaded HTTP request driver for HTTPMR.
+
+Command-line tool for driving HTTPMR operations.  Spawns multiple threads for
+concurrent shard operation, handles statistics collection and operation failure
+retries.
+
+Sample usage:
+
+driver.py --httpmr_base=http://your.app.com/httpmr_base_url \
+    --max_operations_inflight=10 \
+    --max_per_operation_failures=10
+"""
+
 import HTMLParser
 import logging
 import optparse
@@ -16,15 +29,53 @@ INTERMEDIATE_DATA_CLEANUP_MASTER_TASK_NAME = "cleanup_master"
 INFINITE_PARAMETER_VALUE = -1
 
 
+class Error(Exception):
+  """Base class for all driver-specific Exceptions."""
+
+
+class UncrecoverableOperationError(Error):
+  """Base class for all fatal operation errors."""
+
+
+class TooManyTriesError(UncrecoverableOperationError):
+  """An operation has been tried too many times without success."""
+
+
 class OperationResult(object):
+  """Simple data object that holds the result of a map or reduce operation.
+  
+  To use, set public instance parameters directly.  Not meant to be used outside
+  the context of this module.
+  """
   
   def __init__(self):
     self.url = None
     self.next_url = None
+    self.errors = []
+    self.tries = 0
     self.statistics = {}
+  
+  def __str__(self):
+    return str({"url": self.url,
+                "next_url": self.next_url,
+                "errors": self.errors,
+                "tries": self.tries,
+                "statistics": self.statistics})
+  
+  def ParseStatisticsString(self, statistics_string):
+    logging.debug("Parsing statistics from %s" % statistics_string)
+    self.statistics = {}
+    for line in statistics_string.splitlines():
+      tuple = line.split(" ")
+      if len(tuple) == 2:
+        key = tuple[0]
+        value = float(tuple[1])
+        self.statistics[key] = value
+    logging.debug("Got statistics: %s" % self.statistics)
 
 
 class OperationResultHTMLParser(HTMLParser.HTMLParser):
+  """HTMLParser that reads the HTML page from a Map or Reduce operation."""
   
   def handle_starttag(self, tag, attrs):
     if tag == "a":
@@ -33,17 +84,24 @@ class OperationResultHTMLParser(HTMLParser.HTMLParser):
       self.handle_start_pre_tag(attrs)
     
   def handle_start_a_tag(self, attrs):
+    """Determine the next operation's URL."""
     self.url = None
     for tuple in attrs:
       if tuple[0] == "href":
         self.url = tuple[1]
   
   def handle_start_pre_tag(self, attrs):
-    logging.info(attrs)
-    self.statistics = {}
+    """Read the statistics information from the <pre> tag."""
+    self._in_pre_tag = True
+  
+  def handle_data(self, data):
+    if hasattr(self, "_in_pre_tag") and self._in_pre_tag:
+      self.statistics = data
+      self._in_pre_tag = False
 
 
 class MasterPageResultHTMLParser(HTMLParser.HTMLParser):
+  """HTMLParser that reads the HTML page from a Master page."""
 
   def Init(self):
     self.urls = []
@@ -53,16 +111,29 @@ class MasterPageResultHTMLParser(HTMLParser.HTMLParser):
       self.handle_start_a_tag(attrs)
     
   def handle_start_a_tag(self, attrs):
+    """Read the 'href' attribute from an 'a' tag, and add it to the list of URLs
+    
+    The master page for any HTTPMR operation master page lists a set of <a>
+    tags, each representing the first operation of the relevant shard.  Each of
+    these links should be retained and used to populate the initial set of
+    operation threads.
+    """ 
     logging.debug("Reading 'a' tag: %s" % attrs)
     for tuple in attrs:
       if tuple[0] == "href":
         self.urls.append(tuple[1])
 
 
-class TooManyTriesError(Exception): pass
-
-
 class OperationThread(threading.Thread):
+  """An OperationThread handles the execution and retry of an HTTP request.
+  
+  The OperationThread handles executing and retrying an HTTP request to a single
+  Map or Reduce operation.  Once the thread has successfully completed its
+  operation (successfully fetched the url assigned via #SetUrl and parsed the
+  operation result page HTML), the callback set via #SetOperationCallback is
+  invoked.  If there is an unrecoverable error (i.e., too many operation
+  failures), the callback set via #SetUnrecoverableErrorCallback is invoked.
+  """
   
   def SetOperationCallback(self, callback, **kwargs):
     """Set the callback that will be invoked when this operation is finished.
@@ -89,37 +160,60 @@ class OperationThread(threading.Thread):
     self.error_callback_kwargs = kwargs
   
   def SetMaxTries(self, max_tries):
+    """Set the maximum number of tries that the operation can be performed.
+    
+    If the operation is attempted unsuccessfully more than this number of times,
+    the operation is considered to fail and a TooManyTriesError is handed to the
+    unrecoverable error callback.
+    """
     self.max_tries = max_tries
   
   def SetUrl(self, url):
+    """Specify the URL that this operation should operate on."""
     self.url = url
+    self._cancel = False
   
   def run(self):
+    """Fetch the URL, retry on failures, invoke error or operation callbacks."""
     assert self.url is not None
     assert self.operation_callback is not None
     assert self.error_callback is not None
     self.html = None
+
+    logging.info("Starting operation on %s." % self.url)
+    
+    self.results = OperationResult()
+    self.results.url = self.url
+    
     try:
+      if self._cancel:
+        return
       self.html = self._FetchWithRetries(self.url, self.max_tries)
       logging.debug("Retrieved HTML %s" % self.html)
-    except Exception, e:
+    except UncrecoverableOperationError, e:
       self.error_callback(self.url, e, **self.error_callback_kwargs)
-    self.operation_callback(self._GetResults(),
-                            **self.operation_callback_kwargs)
+    self._PopulateResults()
+    if not self._cancel:
+      self.operation_callback(self.results, **self.operation_callback_kwargs)
+  
+  def Cancel(self):
+    self._cancel = True
   
   def _FetchWithRetries(self, url, max_tries):
     tries = 0
     while tries < max_tries or max_tries == INFINITE_PARAMETER_VALUE:
       try:
         tries += 1
+        self.results.tries = tries
         return self._Fetch(url)
       except urllib2.URLError, e:
         logging.warning("URLError on fetch of %s: %s" % (url, str(e)))
+        self.results.errors.append(e)
         self._WaitForRetry(tries)
     raise TooManyTriesError("Too many tries on URL %s" % url)
   
   def _WaitForRetry(self, tries):
-    wait_time_sec = min(tries * 2, 60)
+    wait_time_sec = min(tries * tries, 60)
     logging.debug("Sleeping for %s seconds." % wait_time_sec)
     time.sleep(wait_time_sec)
     
@@ -139,23 +233,22 @@ class OperationThread(threading.Thread):
              parts.fragment)
     return urlparse.urlunsplit(parts)
   
-  def _GetResults(self):
+  def _PopulateResults(self):
     parser = OperationResultHTMLParser()
     parser.feed(self.html)
     parser.close()
     
-    results = OperationResult()
-    results.url = self.url
-    results.next_url = None
+    self.results.next_url = None
     if hasattr(parser, "url"):
-      results.next_url = parser.url
-    results.statistics = None
+      self.results.next_url = parser.url
     if hasattr(parser, "statistics"):
-      result.statistics = parser.statistics
-    return results
+      self.results.ParseStatisticsString(parser.statistics)
   
 
 class HTTPMRDriver(object):
+  
+  threads = []
+  threads_pending = []
   
   def __init__(self,
                httpmr_base,
@@ -173,11 +266,12 @@ class HTTPMRDriver(object):
     self._Map()
 
   def _HandleUnrecoverableOperationError(self, url, error):
-    # TODO(peterdolan): Something more intelligent than just exiting, not sure
-    # what though...
     logging.error("Unrecoverable error on url %s: %s; %s" %
                   (url, type(error), error))
-    sys.exit(1)
+    for thread in HTTPMRDriver.threads:
+      thread.Cancel()
+    logging.info("Going to cleanup.")
+    self._Cleanup()
     
   def _Map(self):
     self._LaunchPhase(MAP_MASTER_TASK_NAME, self._AllMapOperationsComplete)
@@ -200,20 +294,32 @@ class HTTPMRDriver(object):
     
   def _AllCleanupOperationsComplete(self):
     logging.info("Done Cleaning Up!")
+    logging.debug("Results: %s" % self.results)
+    logging.info("Comprehensive Results: %s" % self._GetAggregateResults())
+  
+  def _GetAggregateResults(self):
+    def AddDicts(a, b):
+      sum_dict = {}
+      for key in a:
+        if key in b:
+          sum_dict[key] = a[key] + b[key]
+      return sum_dict
+    return reduce(AddDicts, self.results)
   
   def _LaunchPhase(self, phase_task_name, all_operations_complete_callback):
+    logging.info("Starting %s phase." % phase_task_name)
     base_urls = self._GetInitialUrls(phase_task_name)
     self.threads_inflight = 0
-    self.threads_pending = []
     for url in base_urls:
       thread = self._CreateOperationThread(url,
                                            all_operations_complete_callback)
+      HTTPMRDriver.threads.append(thread)
       if (self.threads_inflight < self.max_operations_inflight or
           self.max_operations_inflight == INFINITE_PARAMETER_VALUE):
         self.threads_inflight += 1
         thread.start()
       else:
-        self.threads_pending.append(thread)
+        HTTPMRDriver.threads_pending.append(thread)
 
   def _GetInitialUrls(self, task):
     url = "%s?task=%s" % (self.httpmr_base, task) 
@@ -246,9 +352,9 @@ class HTTPMRDriver(object):
       logging.debug("Initializing new thread to handle %s" % results.next_url)
       thread = self._CreateOperationThread(results.next_url,
                                            all_operations_complete_callback)
-      self.threads_pending.insert(0, thread)
+      HTTPMRDriver.threads_pending.insert(0, thread)
 
-    if self.threads_pending:
+    if HTTPMRDriver.threads_pending:
       logging.debug("Starting the next pending thread.")
       thread = self.threads_pending.pop()
       self.threads_inflight += 1
